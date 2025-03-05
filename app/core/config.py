828daf16ade0f8fb6205db738debf86e9cd88b45
Husan Ibragimov <lazy.devsloper@gmail.com>
2025-03-05 14:43:45 +0500

    cors

--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     TIMEZONE: str = "Asia/Tashkent"
     DESCRIPTION: str = "This is a Geographic Information System (G.I.S) API. It provides information about regions and districts in Uzbekistan. The API is built using FastAPI and MongoDB. API is powered by Statistics Agency of the Republic of Uzbekistan."
     DEBUG: bool = os.getenv("DEBUG")
-    ALLOWED_ORIGINS: list[str] = os.getenv("ALLOWED_ORIGINS").split(",")
+    ALLOWED_ORIGINS: list[str] = [os.getenv("ALLOWED_ORIGINS")]
     ALLOWED_METHODS: list[str] = ["*"]
     ALLOWED_HEADERS: list[str] = ["*"]
     IS_ALLOWED_CREDENTIALS: bool = os.getenv("IS_ALLOWED_CREDENTIALS")
