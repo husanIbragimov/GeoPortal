9362eaa4e521e15a1e0e768342e2b5646a4e1fc0
Husan Ibragimov <lazy.devsloper@gmail.com>
2025-03-18 10:21:09 +0500

    allowed all

--- app/core/config.py
+++ app/core/config.py
@@ -13,13 +13,7 @@ class Settings(BaseSettings):
     DESCRIPTION: str = "This is a Geographic Information System (G.I.S) API. It provides information about regions and districts in Uzbekistan. The API is built using FastAPI and MongoDB. API is powered by Statistics Agency of the Republic of Uzbekistan."
     DEBUG: bool = os.getenv("DEBUG")
     ALLOWED_ORIGINS: list[str] = [
-        "http://172.16.10.83:8008",
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://172.16.3.149:3000",
-        "http://172.16.3.149:3001",
-        "http://172.16.3.88:3000",
-        "http://172.16.3.88:3001",
+        "*"
     ]
     ALLOWED_METHODS: list[str] = ["*"]
     ALLOWED_HEADERS: list[str] = ["*"]
